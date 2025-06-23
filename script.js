485fea30748e8ba2c33261e37b70b4ce022f28bb
marwaas3d <marwaas3d@gmail.com>
2025-06-23 18:38:41 +0300

    first submit

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById("menuBtn");
+const burgerIcon = document.getElementById("burgerIcon");
+const closeIcon = document.getElementById("closeIcon");
+
+menuBtn.addEventListener("click", function () {
+  burgerIcon.classList.toggle("d-none");
+  closeIcon.classList.toggle("d-none");
+
+});
+
+
+
+/******************************************************************** */
